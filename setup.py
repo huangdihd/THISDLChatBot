e3d122f879c1c92a7de488a5a88a44d0399f5f1c
huangdi <hd20100104@163.com>
2024-06-28 20:51:37 +0800

    更新pip版本号

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 setup(
     name='THISDLChatBot',
-    version='2.0.12',
+    version='2.0.13',
     description='A chatbot framework for THISDL chat rooms',
     url='https://github.com/huangdihd/THISDLChatBot',
     author='huangdihd',
     author_email='hd20100104@163.com',
-    license='GPLv2',
+    license='GPLv3',
     packages=['THISDLChatBot'],
     install_requires=['colorama', 'httpx', 'pillow', 'beautifulsoup4']
 )
