05cffc2db896a0e3af69411f6b61102376ab4f2a
huangdi <hd20100104@163.com>
2024-11-15 22:06:56 +0800

    优化Config系统

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='THISDLChatBot',
-    version='2.0.17',
+    version='2.0.18',
     description='A chatbot framework for THISDL chat rooms',
     url='https://github.com/huangdihd/THISDLChatBot',
     author='huangdihd',
